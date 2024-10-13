9a374b51d911a88f652e5848f66ce9503e4e0ef2
Konica Patait <konicapatait@gmail.com>
2024-10-13 17:26:15 -0600

    Adding file selector

--- src/app/diff-comparator/diff-comparator.component.ts
+++ src/app/diff-comparator/diff-comparator.component.ts
@@ -15,9 +15,50 @@ export class DiffComparatorComponent {
   text1: string = '';
   text2: string = '';
   diffs: any[] = [];
+  fileContent1: string = '';
+  fileContent2: string = '';
+
+  onFile1Selected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.fileContent1 = reader.result as string; // Convert result to string
+      };
+
+      reader.onerror = (error) => {
+        console.error('Error reading file:', error);
+      };
+
+      reader.readAsText(file); // Read the file as text
+    }
+  }
+
+  onFile2Selected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.fileContent2 = reader.result as string; // Convert result to string
+      };
+
+      reader.onerror = (error) => {
+        console.error('Error reading file:', error);
+      };
+
+      reader.readAsText(file); // Read the file as text
+    }
+  }
 
   compare() {
-    const result = this.dmp.diff_main(this.text1, this.text2);
+    console.log("compare text 1: ", this.fileContent1)
+    const result = this.dmp.diff_main(this.fileContent1, this.fileContent2);
     this.dmp.diff_cleanupSemantic(result);
     this.diffs = result;
   }
